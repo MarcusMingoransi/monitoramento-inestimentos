92d8d11ba9cc1238cc3e8de84381effd97811dae
MarcusMingoransi <marcus.mingoransi@outlook.com>
2020-06-04 16:29:47 -0300

    feat: Feito ajustes pra mobile.

--- project/src/app/components/dashboard/dashboard.component.ts
+++ project/src/app/components/dashboard/dashboard.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/shared/api-service.service';
 import { HttpHeaders } from '@angular/common/http';
 import { IStock } from 'src/app/shared/models/IStock';
+
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -11,8 +14,36 @@ export class DashboardComponent implements OnInit {
 
   stock: IStock;
   dataSource = [];
+  cols = 5;
 
-  constructor(private apiService: ApiService) {}
+  breakpoints = {
+    sm: '(max-width: 576px)',
+    md: '(max-width: 768px)',
+    lg: '(max-width: 992px)',
+    xl: '(max-width: 1200px)',
+  };
+
+  constructor(private apiService: ApiService, breakpointObserver: BreakpointObserver) {
+    breakpointObserver.observe([
+      this.breakpoints.sm, this.breakpoints.md, this.breakpoints.lg, this.breakpoints.xl
+    ]).subscribe(result => {
+      console.log(result);
+      if (result.breakpoints[this.breakpoints.sm]) {
+        console.log('sm');
+        this.cols = 1;
+      } else if (result.breakpoints[this.breakpoints.md]) {
+        console.log('md');
+        this.cols = 5;
+      } else if (result.breakpoints[this.breakpoints.lg]) {
+        console.log('lg');
+        this.cols = 5;
+      } else if (result.breakpoints[this.breakpoints.xl]) {
+        console.log('xl');
+        this.cols = 5;
+      }
+      // this.isMobile = result.matches;
+    });
+  }
 
 
 
@@ -30,5 +61,4 @@ export class DashboardComponent implements OnInit {
   onRowClick(stock: IStock) {
     this.stock = stock;
   }
-
 }
